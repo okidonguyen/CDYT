ea1e5926c8bb89a451a463967696a57cd312510c
ptathienlap <111001344+ptathienlap@users.noreply.github.com>
2022-12-03 15:23:41 +0700

    hoat dong noi bo

--- src/pages/Noibo/Hoatdong/index.js
+++ src/pages/Noibo/Hoatdong/index.js
@@ -1,5 +1,36 @@
-function HoatdongNoibo() {
-  return <h1>HoatdongNoibo Page</h1>;
+import { Col, Container, Row } from 'reactstrap';
+import CompanyLink from '~/components/CompanyLink';
+import Majors from '~/components/Majors';
+import News from '~/components/News';
+import Notification from '~/components/Notification';
+import { Slideshow } from '~/components';
+import {items, footerItems} from "~/data";
+import { SlideshowSettings as settings} from '~/data';
+
+function HoatdongNoibo() {    
+    return (
+        <Container fluid className="p-0">            
+            <Slideshow items={items} settings={settings} />            
+            <Container fluid="lg">
+                <Row className="pt-3 pb-3">
+                    <Col className="col-12 col-lg-7 p-0">
+                        <News />
+                    </Col>
+                    <Col className="col-12 col-lg-5 p-0">
+                        <Notification />
+                    </Col>
+                </Row>
+            </Container>
+            <Majors />
+            <Container className='d-none d-lg-block' style={{padding:"30px", textAlign:"center"}}>
+            <h3 className='border-bottom'>HÌNH ẢNH HOẠT ĐỘNG</h3>
+            </Container>
+            <Container className='d-none d-lg-block'>            
+                <Slideshow items={footerItems}  settings={settings} />
+            </Container>
+            <CompanyLink />
+        </Container>
+    );
 }
 
 export default HoatdongNoibo;

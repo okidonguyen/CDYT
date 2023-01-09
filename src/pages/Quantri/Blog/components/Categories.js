d86db47bfddad845d1129f2fdf647a82aa7426a6
HuyTQ <huytq270397@email.com>
2023-01-09 22:47:13 +0700

    - Get categories from db

--- src/pages/Quantri/Blog/components/Categories.js
+++ src/pages/Quantri/Blog/components/Categories.js
@@ -1,54 +1,71 @@
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css"; 
 import { MultiSelect } from 'primereact/multiselect';
-import { Skeleton } from 'primereact/skeleton';
-import { classNames } from 'primereact/utils';
+// import { Skeleton } from 'primereact/skeleton';
+// import { classNames } from 'primereact/utils';
+import axios from 'axios';
 
 const MultiSelectDemo = (props) => {
-
-    const [lazyItems, setLazyItems] = useState([]);
-    const [lazyLoading, setLazyLoading] = useState(false);
-    const [selectedCities1, setSelectedCities1] = useState(null);
-    const [selectedCities2, setSelectedCities2] = useState(null);
-    const [selectedCountries, setSelectedCountries] = useState(null);
-    const [selectedGroupedCities, setSelectedGroupedCities] = useState(null);
-    const [selectedItems1, setSelectedItems1] = useState(null);
-    const [selectedItems2, setSelectedItems2] = useState(null);
-    const [selectAll, setSelectAll] = useState(false);
-    const [items] = useState(Array.from({ length: 100000 }).map((_, i) => ({ label: `Item #${i}`, value: i })));
-    const loadLazyTimeout = useRef(null);
-
-    const categories = [
-        {id: 1, title: "cat 1"},
-        {id: 2, title: "cat 2"},
-        {id: 3, title: "cat 3"},
-        {id: 4, title: "cat 4"},
-        {id: 5, title: "cat 5"},
-    ];
+    const host = "http://localhost:3001";
+    // const [lazyItems, setLazyItems] = useState([]);
+    // const [lazyLoading, setLazyLoading] = useState(false);
+    // const [selectedCities1, setSelectedCities1] = useState(null);
+    // const [selectedCities2, setSelectedCities2] = useState(null);
+    // const [selectedCountries, setSelectedCountries] = useState(null);
+    // const [selectedGroupedCities, setSelectedGroupedCities] = useState(null);
+    // const [selectedItems1, setSelectedItems1] = useState(null);
+    // const [selectedItems2, setSelectedItems2] = useState(null);
+    // const [selectAll, setSelectAll] = useState(false);
+    // const [items] = useState(Array.from({ length: 100000 }).map((_, i) => ({ label: `Item #${i}`, value: i })));
+    // const loadLazyTimeout = useRef(null);
 
     const [selectedItems, setSelectedItems] = useState([]);
+    const [options, setOptions] = useState([]);
 
-    useEffect(() => {
-        setLazyItems(Array.from({ length: 100000 }));
-        setLazyLoading(false)
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    const getCategories = () => {
+        axios.get(`${host}/api/admin/categories`)
+          .then(response => response)
+          .then(({data: res}) => {
+            if (res?.success) {
+                const cats = res?.data?.map(({id, title}) => ({
+                    id: id,
+                    title: title
+                  }));
+                  setOptions(cats);
+            }
+          })
+          .catch(err => {
+            console.log("Lỗi kết nối server!");
+          })
+      }
     
+      useEffect(() => {
+        getCategories()
+      }); 
+
     const onSelectedChange = (value) => {
         setSelectedItems(value);
         props.onSelectedItems(value)
     }
 
+    const resetValue = () => {
+        setSelectedItems(props.value)
+    }
+
+    useEffect(() => resetValue());
+
     return (
         <div>
             <MultiSelect 
                 value={selectedItems} 
-                options={categories} 
+                options={options} 
                 onChange={(e) => onSelectedChange(e.value)} 
                 optionLabel="title" 
                 className="w-100"
+                placeholder='Chọn danh mục'
              />
         </div>  
     );
